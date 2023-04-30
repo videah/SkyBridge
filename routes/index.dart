3013ac943f0280fd7b5b80c51b3040228c971f4c
videah <videah@selfish.systems>
2023-04-30 06:05:29 +0100

    delete default dart_frog tests

--- routes/index.dart
+++ routes/index.dart
@@ -1,5 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+  return Response.json(
+    body: {},
+  );
 }
