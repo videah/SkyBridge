b251840d17e78f26068b58e28c598176dccb04e2
videah <videah@selfish.systems>
2023-06-23 14:01:05 -0500

    render first quote post image as card attachment

--- lib/models/mastodon/mastodon_card.dart
+++ lib/models/mastodon/mastodon_card.dart
@@ -81,12 +81,34 @@ class MastodonCard {
     var handle = '@unknown.bsky.social';
     var clickableUrl = base;
 
+    // Ivory expects an image to render a card so we pass a 1x1 transparent
+    // image to make it happy.
+    var quoteImage = 'https://$base/1px.png';
+    var useAttachedMedia = false;
+
     // Get any data we need from the post's record.
     record.record.map(
       record: (post) {
         handle = post.data.author.handle;
         title = 'Quote Post - (@$handle) \n ${post.data.value.text}';
         clickableUrl = 'https://$base/@$handle/${dbRecord.id}';
+
+        // If the record has a media attachment, we can use that instead.
+        final embeds = post.data.embeds;
+        if (embeds != null) {
+          for (final embed in embeds) {
+            embed.map(
+              record: (_) {},
+              images: (images) {
+                quoteImage = images.data.images.first.fullsize;
+                useAttachedMedia = true;
+              },
+              external: (_) {},
+              recordWithMedia: (_) {},
+              unknown: (_) {},
+            );
+          }
+        }
       },
       notFound: (_) {},
       blocked: (_) {},
@@ -94,9 +116,6 @@ class MastodonCard {
       unknown: (_) {},
     );
 
-    // Ivory expects an image to render a card so we pass a 1x1 transparent
-    // image to make it happy.
-    final quoteImage = 'https://$base/1px.png';
     return MastodonCard(
       url: clickableUrl,
       title: title,
@@ -107,8 +126,8 @@ class MastodonCard {
       providerName: '',
       providerUrl: '',
       html: '',
-      width: 1000,
-      height: 1,
+      width: useAttachedMedia ? 864 : 1000,
+      height: useAttachedMedia ? 432 : 1,
       embedUrl: quoteImage,
       image: quoteImage,
     );
